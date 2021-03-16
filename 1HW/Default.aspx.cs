01f5e58f83c121b4d26e9ecac340a4e7e000d2ef
何俊賢 <geass0cool@gmail.com>
2021-03-16 16:05:58 +0800

    作業1

--- 1HW/Default.aspx.cs
+++ 1HW/Default.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace _1HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            
         }
     }
 }
